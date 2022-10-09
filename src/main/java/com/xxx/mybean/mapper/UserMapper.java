6f906eb7e23dccf838cd40a16acb83008b40c3f6
stillwho <1115015347@qq.com>
2022-10-09 15:51:14 +0800

    second

--- src/main/java/com/xxx/mybean/mapper/UserMapper.java
+++ src/main/java/com/xxx/mybean/mapper/UserMapper.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserMapper {
     public boolean selectByUserId(String username,String password){
-
+        System.out.println("tijiao");
         System.out.println("dao");
         return true;
     }
